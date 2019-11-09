37e51cef14c092b9c8cbc2cfe54d7563d5d0ca13
catehulu <sirajjaya@gmail.com>
2019-11-09 19:06:54 +0700

    first commit

---
+++ app/src/main/java/com/example/lightsensor/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.lightsensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView textLIGHT_available, textLIGHT_reading;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textLIGHT_available
+                = (TextView)findViewById(R.id.lightAvailable);
+        textLIGHT_reading
+                = (TextView)findViewById(R.id.lightReading);
+
+        SensorManager mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        Sensor lightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(lightSensor != null){
+            textLIGHT_available.setText("Sensor.TYPE_LIGHT Available");
+            mySensorManager.registerListener(
+                    lightSensorListener,
+                    lightSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+        } else {
+            textLIGHT_available.setText("Sensor.TYPE_LIGHT NOT Available");
+        }
+    }
+
+    private final SensorEventListener lightSensorListener
+            = new SensorEventListener(){
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+                textLIGHT_reading.setText("LIGHT: " + event.values[0]);
+            }
+        }
+
+    };
+
+}
